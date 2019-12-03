c5fdfb65572858b94e10ab05475379017c01f3ad
Andras Zsolt Tisza <andras.tisza@ni.com>
2019-12-03 12:00:27 +0100

    - add manual unit tests

--- src/main/java/org/pub/whitebox/SimpleGraph.java
+++ src/main/java/org/pub/whitebox/SimpleGraph.java
@@ -1,5 +1,16 @@
 package org.pub.whitebox;
 
 public class SimpleGraph {
-
+  private SimpleGraph() {}
+  
+  public static int simpleFunction(int a, int b, int c) {
+    if (a == 354) {
+      if (b > c) {
+        a = b;
+      } else {
+        a = c;
+      }
+    }
+    return a;
+  }
 }
